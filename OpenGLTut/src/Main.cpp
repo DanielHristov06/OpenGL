e6bb3a79188a910b2465aa23fbd298b8fa3be5c6
TheEliteD <dhristov06@gmail.com>
2024-11-10 17:39:10 +0200

    Created a window

--- OpenGLTut/src/Main.cpp
+++ OpenGLTut/src/Main.cpp
@@ -1,25 +1,40 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 
 int main() {
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		return -1;
-	}
+	const int width = 1280;
+	const int height = 720;
 
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1290, 720, "Wow, a window.", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "Wow, a window.", NULL, NULL);
 
 	if (window == NULL) {
+		std::cout << "Failed to create a window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
