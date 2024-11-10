8fb55919d42e09c2f5342d4553ac1bd721c8d836
TheEliteD <dhristov06@gmail.com>
2024-11-10 22:00:27 +0200

    First triangle
    Made a createShader and a createProgram functions for easier use

--- OpenGLTut/src/Main.cpp
+++ OpenGLTut/src/Main.cpp
@@ -8,15 +8,64 @@ const unsigned int WIDTH = 1280;
 const unsigned int HEIGHT = 720;
 
 // Whenever the window size changes (by OS or by the user) this callback function gets called
-void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
 // Process all input
-void processInput(GLFWwindow *window) {
+static void processInput(GLFWwindow *window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+}
+
+// A function to create a shader, not matter what type
+static unsigned int createShader(std::string& source, unsigned int shaderType) {
+	// Shader creation
+	unsigned int shader = glCreateShader(shaderType);
+	// Convert the standart string to const char* (c style string)
+	const char* src = source.c_str();
+
+	// Compile the shader
+	glShaderSource(shader, 1, &src, NULL);
+	glCompileShader(shader);
+
+	// Print any error if the compilation fails
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "Failed to compile " << (shaderType == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader" << std::endl;
+		std::cout << infoLog << std::endl;
+		glDeleteShader(shader);
+	}
+
+	return shader;
+}
+
+// A function to create a program with a vertex and a fragment shader
+static unsigned int createProgram(std::string& vertexShader, std::string& fragmentShader) {
+	// Program creation
+	unsigned int shaderProgram = glCreateProgram();
+
+	// Creating the two shaders
+	unsigned int VS = createShader(vertexShader, GL_VERTEX_SHADER);
+	unsigned int FS = createShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+	//Attaching the shaders to the progtam
+	glAttachShader(shaderProgram, VS);
+	glAttachShader(shaderProgram, FS);
+
+	// Linking the program
+	glLinkProgram(shaderProgram);
+
+	// Deleting the shaders (we don't need tghem anymore)
+	glDeleteShader(VS);
+	glDeleteShader(FS);
+
+	return shaderProgram;
 }
 
 int main() {
@@ -52,19 +101,62 @@ int main() {
 		return -1;
 	}
 
+	float vertices[9] = {
+		-0.5f, -0.5f, 0.f,
+		 0.5f, -0.5f, 0.f,
+		  0.f,  0.5f, 0.f
+	};
+
+	// Creattes and binds a vertex buffer object
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Creates and binds a vertex array object
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, NULL);
+	glEnableVertexAttribArray(0);
+
+	std::string vertexShader = "#version 460 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main() {\n"
+		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n";
+
+	std::string fragmentShader = "#version 460 core\n"
+		"out vec4 FragColor;\n"
+		"void main() {\n"
+		"FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
+		"}\n";
+
+	unsigned int shaderProgram = createProgram(vertexShader, fragmentShader);
+
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
 		// Input
 		processInput(window);
 
 		// Render
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Setting the shader program (which one to use)
+		glUseProgram(shaderProgram);
+		// Drawing the triangles
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// Swap buffers and poll IO events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// Delete the shader program
+	glDeleteProgram(shaderProgram);
 
 	//Terminate GLFW
 	glfwTerminate();
